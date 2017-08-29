b6115713e79f1f5fe07985b36f973ae2e3bcde01
Michal Gruszka <m.gruszka215@gmail.com>
2017-08-29 16:21:13 +0200

    implement add_to_graphic function

--- game.py
+++ game.py
@@ -1,20 +1,14 @@
-from graphics.tiles.py import Tiles
-from graphics import graphictools
+import tiletools
+import graphictools
 
 
-
-
-
-
-
-
-
-
-
-
-
+def main():
+    interface = graphictools.import_graphic_from_file('graphics/interface.gfx', 80, 23)
+    map1 = graphictools.import_graphic_from_file('graphics/map1.gfx', 47, 21)
+    display = graphictools.add_to_graphic(interface, map1, 1, 1)
+    graphictools.print_graphic(display)
 
 
 
 if __name__ == '__main__':
-    print(Tiles.grass.string)
+        main()
