49738ca6b1e533a031a7c37edf0ea5e2c4049f3a
siemaeniu500 <siemaeniu500@gmail.com>
2017-08-31 09:41:27 +0200

    add displaying inventory and handling map2

--- game.py
+++ game.py
@@ -17,10 +17,40 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def map_2_handler(hero):
+def prepare_inventory_list(inventory):
+    line_list = []
+    item_info_length = 0
+    name_max_length = 0
+    for item in inventory:
+        if (len(item) > name_max_length):
+            name_max_length = len(item)
+        if (len(str(inventory.__getitem__(item))) > item_info_length):
+            item_info_length = len(str(inventory.__getitem__(item)))
+    line_list.append("Name         amount weight")
+    item_info_length += 2;
+    name_max_length -= 5
+    format1 = str("{:<" + str(item_info_length) + "s}")
+    format2 = str("{:<" + str(name_max_length) + "s}")
+    for item in inventory:
+        format3 = "{}"
+        item_info = inventory.__getitem__(item)
+        line = str(format1 + format2 + format3).format(item, str(item_info[0]), str(item_info[1]))
+        line_list.append(line)
+    return line_list
+
+def add_to_inventory(item,amount,weight):
+    if tiletools.Hero.inventory.__contains__("item"):
+        item_info = tiletools.Hero.inventory[item]
+        item_amount = item_info[1] + float(amount)
+        item_weight = item_info[2] + float(weight)
+        tiletools.Hero.inventory[item] = [item_amount , item_weight]
+    else:
+        tiletools.Hero.inventory.__setitem__(item, [float(amount), float(weight)])
+
+def map_2_handler():
     interface = graphictools.import_graphic_from_file('graphics/interface.gfx', 80, 23)
-    current_map = maptools.Maps.map2
-    hero = tiletools.Hero(100, 50, 4, 'up')
+    current_map = maptools.Map('map2', 'graphics/map1.gfx')
+    hero = tiletools.Hero(100, 10, 10, 'up')
     display = graphictools.add_to_graphic(interface, current_map.map_graphic, 1, 1)
     game_loop_2(interface, current_map, display, hero)
 
@@ -44,21 +74,45 @@ def game_loop_1(interface, current_map, display, hero):
 
         handle_user_input(display, current_map, key_pressed, hero)
 
+
         if hero.y == 1 and current_map.name == "map1":
             current_map = maptools.Maps.map2
             hero.y = 21
 
 
 def game_loop_2(interface, current_map, display, hero):
+    info_box_message = ['Prepare yourself for','the final fight.', 'Gather all items marked', 'as "?" on the map.']
     while True:
+        hero_string_pos = str(hero.x) + ',' + str(hero.y)
+        if (maptools.Maps.items.__contains__(hero_string_pos)):
+            item = maptools.Maps.items[hero_string_pos].split(',')
+            add_to_inventory(item[0], item[1], item[2])
+            graphictools.add_dialogue_to_display(interface, graphictools.get_dialogue_graphic(
+                [[tiletools.Tiles.black.string] * 28] * 9))
+            info_box_message = ['You gained:', str('name: ' + item[0]), str('amount: ' + item[1])]
+            current_map.map_graphic[hero.x - 1][hero.y -1 ] = tiletools.Tiles.grass
+            maptools.Maps.items.__delitem__(hero_string_pos)
+            if(not maptools.Maps.items):
+                graphictools.add_dialogue_to_display(interface, graphictools.get_dialogue_graphic(
+                    [[tiletools.Tiles.black.string] * 28] * 9))
+                info_box_message = ['You are ready for the battle',
+                                    'get to the boss cave',
+                                    'using black entrance',
+                                    'at the bottom of the map']
+            print(tiletools.Hero.inventory)
+
         display = graphictools.add_to_graphic(interface, current_map.map_graphic, 1, 1)
-        graphictools.add_dialogue_to_display(interface, graphictools.get_dialogue_graphic())
+        graphictools.add_dialogue_to_display(interface, graphictools.get_dialogue_graphic(info_box_message))
         display = graphictools.add_single_tile_to_graphic(display, hero, hero.x, hero.y)
         graphictools.print_graphic(display)
 
         key_pressed = getch()
+        if (key_pressed == 'i' or key_pressed == 'i'):
+            info_box_message = prepare_inventory_list(hero.inventory)
 
         handle_user_input(display, current_map, key_pressed, hero)
+        if(not maptools.Maps.items and hero_string_pos in ['13,18', '14,18', '15,18']):
+            exit()
 
 
 
@@ -134,7 +188,7 @@ def selected_item_handler(option):
     :return: None
     """
     if (option == 1):
-        map_1_handler()
+        map_2_handler()
     if (option == 5):
         exit()
         
--- maptools.py
+++ maptools.py
@@ -1,31 +1,29 @@
 import graphictools
 class Map():
-    def __init__(self, name, map_graphic):
+    def __init__(self, name, graphic_path, hero=None):
         self.name = name
-        self.map_graphic = map_graphic
+        self.map_graphic = graphictools.import_graphic_from_file(graphic_path, 47, 21)
+        self.hero = hero
 
 
 class Maps():
-    items = {'0,6': 'money,20 ',
-             '1,40': 'blue potion,4',
-             '2,14' : 'red potion,5',
-             '3,2': 'money,50',
-             '5,17': 'golden chestplate,1',
-             '7,32': 'money,89',
-             '8,41': 'golden bow,1',
-             '9,7': 'golden shoes,1',
-             '9,26': 'golden helmet,1',
-             '11,0': 'golden bracelet,1',
-             '11,17': 'golden crossbow,1',
-             '13,34': 'golden arrow,23',
-             '14,20': 'money,55',
-             '16,11': 'blue potion,12',
-             '16,28': 'red potion,6',
-             '17,1': 'speed potion,9',}
+    items = {'7,1': 'gold,20,0.1',
+             '41,2': 'blue potion,4,0.8',
+             '15,3' : 'red potion,5,1',
+             '3,4': 'gold,50,0.3',
+             '18,6': 'gold armor,1,5',
+             '33,8': 'gold,89,0.5',
+             '41,9': 'red potion,9,1.8',
+             '8,10': 'gold shoes,1,3',
+             '27,10': 'gold helmet,1,2',
+             '1,12': 'blue potion,6,1.2',
+             '18,12': 'gold,100,0.5',
+             '35,14': 'arrow,23,0.5',
+             '21,15': 'gold,55,0.2',
+             '12,17': 'blue potion,12,2.4',
+             '2,18': 'gold,40,0.2',
+             '29,17': 'dragon glass,4,1'}
 
-    def load_map_ghraphic(path):
+    def load_map_graphic(path):
         return graphictools.import_graphic_from_file(path, 47, 21)
-
-    map1 = Map('map1', load_map_ghraphic('graphics/map2.gfx'))
-    map2 = Map('map2', load_map_ghraphic('graphics/map1.gfx'))
     
